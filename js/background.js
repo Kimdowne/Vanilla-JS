17167e75c121d3e4a909af7518dcb9d3969a292f
Kimdowne <jun256900@gmail.com>
2023-10-09 13:00:38 +0900

    Created and Updated Momentum
    Progressing 86% with the Lecture

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["windowsXP.jpg", "windows7.jpg", "windows10.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
---
+++ js/greeting.js
@@ -0,0 +1,34 @@
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginInputText = loginForm.querySelector("input:first-child");
+
+const greeting = document.querySelector("h1");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function handleLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username);
+}
+
+function paintGreeting(username) {
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hi! ${username}`;
+}
+
+function handlePlaceholder() {
+  loginInputText.placeholder = "";
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", handleLoginSubmit);
+} else paintGreeting(savedUsername);
+
+loginInputText.addEventListener("click", handlePlaceholder);
